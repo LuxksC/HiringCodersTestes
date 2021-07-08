26a40453ec0010c07d931643a75812746811e806
Sandra Monteiro de Castro <SandraCastro@MacBook-Pro-de-Lucas.local>
2021-07-08 00:50:37 -0300

    ToDoList - Finalizado HTML e iniciado JS

---
+++ ToDoList-Project/ToDoList.js
@@ -0,0 +1,5 @@
+//Criação de constantes que fazem referência aos elementos do HTML
+//para capturar um elemento do HTML, iremos usar o comando document.querySelector e passar como referência o identificador atribuído a esse elemento
+const listContainer = document.querySelector(`[data-lists]`) //div fantasma
+const newListForm = document.querySelector(`[data-new-list-form]`)//formulário
+const newListInput = document.querySelector(`[data-new-list-input]`)//input
